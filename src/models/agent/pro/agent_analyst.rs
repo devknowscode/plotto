21fd49d97c74c86c936e726e4cd666281cb356ac
Phat <nguyenlyphat301202@gmail.com>
2024-02-21 23:55:17 +0700

    Implement general functions for agent analyst

--- src/models/agent/pro/agent_analyst.rs
+++ src/models/agent/pro/agent_analyst.rs
@@ -1,13 +1,21 @@
+use std::time::Duration;
+
 use crate::{
-    helper::general::ai_task_request,
+    helper::{
+        command_line::AgentCommand,
+        general::{ai_task_request, check_status_code},
+    },
     models::agent::basic::{
         basic_agent::{AgentState, BasicAgent},
         basic_trait::BasicTrait,
     },
     tasks::analyst::{print_project_scope, print_site_urls},
 };
+use async_trait::async_trait;
+use crossterm::style::Stylize;
+use reqwest::Client;
 
-use super::pro_trait::{ProjectScope, TaskList};
+use super::pro_trait::{GeneralAgent, ProjectScope, TaskList};
 
 #[derive(Debug)]
 pub struct AgentAnalyst {
@@ -18,7 +26,7 @@ impl AgentAnalyst {
     pub fn new() -> Self {
         let attributes = BasicAgent::new(
             String::from("Gather information and design solution for website development"),
-            String::from("Project Manager"),
+            String::from("Analyst"),
         );
 
         Self { attributes }
@@ -62,6 +70,91 @@ impl AgentAnalyst {
 
         tasklist.external_urls = Some(external_urls);
         self.attributes.update_state(AgentState::Testing);
+    }
+}
+
+#[async_trait]
+impl GeneralAgent for AgentAnalyst {
+    fn get_attributes(&self) -> &BasicAgent {
+        &self.attributes
+    }
+
+    async fn execute(&mut self, tasklist: &mut TaskList) -> Result<(), Box<dyn std::error::Error>> {
+        while self.attributes.state != AgentState::Done {
+            match self.attributes.state {
+                AgentState::Planning => {
+                    let project_scope: ProjectScope = self.define_project_scope(tasklist).await;
+
+                    // Check project scope has external urls
+                    if project_scope.is_external_urls_required {
+                        self.determine_external_urls(
+                            tasklist,
+                            tasklist.description.clone().as_str(),
+                        )
+                        .await;
+                        self.attributes.state = AgentState::Testing;
+                    }
+                }
+                AgentState::Testing => {
+                    // Exclude urls require api key
+                    let mut exclude_urls: Vec<String> = vec![];
+
+                    // Create request to disconnect after 5 seconds
+                    let client: Client = Client::builder()
+                        .timeout(Duration::from_secs(5))
+                        .build()
+                        .unwrap();
+
+                    let urls = tasklist
+                        .external_urls
+                        .as_ref()
+                        .expect("No URLs in task list");
+
+                    // Push url not working to exclude_urls
+                    for url in urls {
+                        // Print agent command
+                        let endpoint_str = format!("Testing URL Endpoint: {}", url);
+                        AgentCommand::Test
+                            .print_agent_message(&self.attributes.position, &endpoint_str);
+
+                        // Test status url
+                        let status_code = check_status_code(&client, url).await;
+                        match status_code {
+                            Ok(code) => {
+                                if code != 200 {
+                                    exclude_urls.push(url.clone());
+                                    println!("{} ❌", format!("Fail::{}", 404).red().bold());
+                                } else {
+                                    println!("{} ✅", format!("Pass::{}", 200).green().bold());
+                                }
+                            }
+                            Err(error) => {
+                                println!("Error checking {}", error);
+                            }
+                        }
+                    }
+
+                    // Exclude any faulty urls
+                    if exclude_urls.len() > 0 {
+                        let new_urls: Vec<String> = tasklist
+                            .external_urls
+                            .as_ref()
+                            .unwrap()
+                            .iter()
+                            .filter(|url| !exclude_urls.contains(url))
+                            .cloned()
+                            .collect();
+
+                        tasklist.external_urls = Some(new_urls);
+                    }
+
+                    self.attributes.state = AgentState::Done;
+                }
+                _ => self.attributes.state = AgentState::Done,
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -121,5 +214,35 @@ mod tests {
             }
             None => assert!(false),
         };
+    }
+
+    #[tokio::test]
+    async fn test_execute_agent_architect() {
+        let mut tasklist: TaskList = TaskList {
+            description: String::from("build a website that tracks forex and crypto prices"),
+            project_scope: None,
+            external_urls: None,
+            backend_code: None,
+            api_endpoint_schema: None,
+        };
+
+        let mut agent_analyst = AgentAnalyst::new();
+        let agent_res = agent_analyst.execute(&mut tasklist).await;
+        match agent_res {
+            Ok(()) => {
+                println!("Done work!")
+            }
+            Err(error) => {
+                println!("Error checking: {}", error);
+            }
+        }
+    }
+
+    #[test]
+    fn test_print_out() {
+        println!("{} ❌", format!("FAIL::{}", 404).red().bold());
+        println!("{} ❌", format!("FAIL::{}", 404).red().bold());
+        println!("{} ✅", format!("PASS::{}", 200).green().bold());
+        println!("{} ✅", format!("PASS::{}", 200).green().bold());
     }
 }
