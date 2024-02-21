f662123515ec84cf21cacc20d39ea450a9080a70
Phat <nguyenlyphat301202@gmail.com>
2024-02-21 17:33:14 +0700

    Add function_to_string to lib

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,20 @@ mod utils;
 fn main() {
     get_user_input("What are we building today?");
 }
+
+#[cfg(test)]
+mod tests {
+    use lib::function_to_string;
+
+    #[function_to_string]
+    fn this_is_test(param: &str) {
+        /// Comment in this test
+        println!("{}", OUTPUT);
+    }
+
+    #[test]
+    fn test_function_to_string() {
+        let fn_str = this_is_test("Blah blah blah...");
+        println!("{:#?}", fn_str);
+    }
+}
